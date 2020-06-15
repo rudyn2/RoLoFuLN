6866b6ef59e8f364a42e1c72200490ebd3885c82
Rudy <c01010010@hotmail.com>
2020-06-15 17:19:37 -0400

    initial structure from cookiecutter https://github.com/drivendata/cookiecutter-data-science

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Robust loss functions under label noise',
+    author='RT@EL7024',
+    license='MIT',
+)
